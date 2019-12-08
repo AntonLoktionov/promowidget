07a51a802a3a399b1485ca7746fe86bad1644cb7
Antonio <antoniopin41@gmail.com>
2019-12-08 18:01:24 +0300

    Updates

---
+++ precache-manifest.0a5f5420b90c806c2ed6220c1ae9f268.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8270d90c5a4947564665",
+    "url": "/promowidget/static/css/main.0b9efa30.chunk.css"
+  },
+  {
+    "revision": "8270d90c5a4947564665",
+    "url": "/promowidget/static/js/main.0ebde674.chunk.js"
+  },
+  {
+    "revision": "900b24629ce023bb5918",
+    "url": "/promowidget/static/js/runtime~main.09984a88.js"
+  },
+  {
+    "revision": "9ffa0bb39bf2ae8148f0",
+    "url": "/promowidget/static/css/2.cf832e01.chunk.css"
+  },
+  {
+    "revision": "9ffa0bb39bf2ae8148f0",
+    "url": "/promowidget/static/js/2.3b25df65.chunk.js"
+  },
+  {
+    "revision": "cfe7cac829aadfb73fb8626ee603536a",
+    "url": "/promowidget/index.html"
+  }
+];
